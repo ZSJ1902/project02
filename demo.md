82a719b356569514e1b98b79289255514f6f0204
zsg <1479322037@qq.com>
2020-03-23 09:17:55 +0800

    use h2 database

---
+++ demo.md
@@ -0,0 +1,10 @@
+开发步骤
+1.先写entity层
+2.有工具类先写工具类
+3.开发controller层
+4.开发service层
+5.开发impl层
+6.开发dao层
+7.开发mapper.xml层
+
+有model，mapper，controller，jdbc的properties配置就可以完成数据库和idea链接功能
--- src/main/java/life/majiang/community/CommunityApplication.java
+++ src/main/java/life/majiang/community/CommunityApplication.java
@@ -1,9 +1,11 @@
 package life.majiang.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class CommunityApplication {
 
     public static void main(String[] args) {
