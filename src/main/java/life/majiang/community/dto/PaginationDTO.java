4654ddf6ec6a7d992077d32a035c284b13e56fbe
zsg <1479322037@qq.com>
2020-04-16 15:32:01 +0800

    interceptor

--- src/main/java/life/majiang/community/dto/PaginationDTO.java
+++ src/main/java/life/majiang/community/dto/PaginationDTO.java
@@ -15,20 +15,10 @@ private boolean showEndPage;
 private Integer page;
 private List<Integer> pages = new ArrayList<>();
 private Integer totalPage;
-public void setPagnition(Integer totalCount, Integer page, Integer size) {
+public void setPagnition(Integer totalPage, Integer page) {
 
 
-    if(totalCount%size==0){
-        totalPage = totalCount/size;
-    }else {
-        totalPage = totalCount/size+1;
-    }
-    if(page<1){
-        page=1;
-    }
-    if(page>totalPage){
-        page=totalPage;
-    }
+    this.totalPage=totalPage;
     this.page=page;
 pages.add(page);
 for(int i =1;i<=3;i++){
---
+++ src/main/java/life/majiang/community/interceptor/WebConfig.java
@@ -0,0 +1,21 @@
+package life.majiang.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+//@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private SeesionInterceptor seesionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(seesionInterceptor).addPathPatterns("/**");
+    }
+
+}
