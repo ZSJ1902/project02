e6415fe76a2a9d15ad12866cf06aa4394e5e00e4
zsg <1479322037@qq.com>
2020-04-27 18:21:48 +0800

    page excption handle

--- src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
+++ src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
@@ -1,15 +1,29 @@
 package life.majiang.community.exception;
 
 public enum CustomizeErrorCode implements ICustomizeErrorCode {
-    QUESTION_NOT_FOUND("你找的问题不存在！");
+    QUESTION_NOT_FOUND(2001,"你找的问题不存在！"),
+    TARGET_PARAM_NOT_FOUND(2002,"未选中任何问题或评论进行回复"),
+    NO_LOGIN(2003,"当前操作需要登录，请登录后重试"),
+    SYS_ERROR(2004,"服务器出问题了，请稍后重试！"),
+    TYPE_PARAM_WRONG(2005,"评论类型错误或不存在！"),
+    COMMENT_NOT_FOUND(2006,"您操作的评论不存在，请换一个试试！"),
+    ;
 
     @Override
     public String getMessage() {
         return message;
     }
+
+    @Override
+    public Integer getCode() {
+        return code;
+    }
+
+    private Integer code;
     private String message;
 
-    CustomizeErrorCode(String message) {
+    CustomizeErrorCode(Integer code, String message) {
+        this.code = code;
         this.message = message;
     }
 }
