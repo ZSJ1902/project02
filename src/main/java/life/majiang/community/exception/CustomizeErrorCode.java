cddecc022b772e518350909d81a583f07f15b502
zsg <1479322037@qq.com>
2020-04-29 18:27:20 +0800

    优化回复和详情功能

--- src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
+++ src/main/java/life/majiang/community/exception/CustomizeErrorCode.java
@@ -7,6 +7,7 @@ public enum CustomizeErrorCode implements ICustomizeErrorCode {
     SYS_ERROR(2004,"服务器出问题了，请稍后重试！"),
     TYPE_PARAM_WRONG(2005,"评论类型错误或不存在！"),
     COMMENT_NOT_FOUND(2006,"您操作的评论不存在，请换一个试试！"),
+    CONTENT_IS_EMPTY(2007,"输入内容不能为空！"),
     ;
 
     @Override
